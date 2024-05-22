2505931e51f17cd83040305157d3192591a5bed2
ss0526100 <ss0526100@gmail.com>
2024-05-22 22:49:59 +0900

    feat: 주문확인 페이지 템플릿 구현

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,18 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import ConfirmOrderPage from './page/ConfirmOrderPage/ConfirmOrderPage';
 import ENDPOINTS from './constants/endpoints';
-import ShoppingCartPage from './page/ShoppingCartPage';
+import OrderConfirmationPage from './page/OrderConfirmationPage/OrderConfrimationPage';
+import ShoppingCartPage from './page/ShoppingCartPage/ShoppingCartPage';
 import { Suspense } from 'react';
 
 const router = createBrowserRouter([
   {
     path: ENDPOINTS.shoppingCart,
     element: <ShoppingCartPage />,
+  },
+  {
+    path: ENDPOINTS.orderConfirmation,
+    element: <OrderConfirmationPage />,
   },
   {
     path: ENDPOINTS.confirmOrder,
